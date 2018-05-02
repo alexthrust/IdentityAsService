0ea3db732ed1849bc5d0ac094208220ea205e32e
Alexey Blauta <alexthrust@gmail.com>
2018-05-03 01:53:10 +0300

    Demo

---
+++ MvcClient/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = "mvc";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code id_token";
+
+                    options.SaveTokens = true;
+                    //options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.ClaimActions.MapCustomJson("roles", jobj =>
+                    {
+                        IEnumerable<string> values = jobj["role"].Values<string>();
+                        StringBuilder sb = new StringBuilder();
+                        foreach (string val in values)
+                        {
+                            sb.Append(val + ",");
+                        }
+                        return sb.ToString().TrimEnd(',');
+                    });
+
+                    options.Scope.Add("api1");
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    options.Scope.Add("roles");
+                    options.Scope.Add("offline_access");
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
